2279a6bfbbf444edc63260dea0c8d5bfdb203dc0
alqudami <8kode8@gmail.com>
2026-09-03 01:13:41 +0300

    Add Translate Folder

---
+++ murasalat_office/murasalat_office/doctype/murasalat_correspondence_type/murasalat_correspondence_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, QupNext and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Murasalat Correspondence Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
