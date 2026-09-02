410b9230a848c8208cc99be46165890022a75943
alqudami <8kode8@gmail.com>
2026-09-02 04:02:24 +0300

    Fix WorkSpace Hidden Or Deleting

--- murasalat_office/hooks.py
+++ murasalat_office/hooks.py
@@ -8,39 +8,12 @@ app_license = "mit"
 
 
 fixtures = [
-    
     {
-        "dt": "Workspace",
-        "filters": [["name", "like", "Murasalat Office%"]]
-        # "filters": [["module", "=", "GSW Procurement"]]
-        
- 
-    },
-        {
         "dt": "Workspace Sidebar",
         "filters": [["name", "like", "Murasalat Office%"]]
-        
     },
-
     {
         "dt": "Desktop Icon",
         "filters": [["name", "like", "Murasalat Office%"]]
-        
     }
-
-    # {
-    #     "dt": "Custom Field",
-    #     "filters": [["name", "like", "Murasalat Office%"]]
-        
-    # },
-    # {
-    #     "dt": "Custom Script",
-    #     "filters": [["name", "like", "GESW%"]]
-        
-    # },
-    # {
-    #     "dt": "Property Setter",
-    #     "filters": [["name", "like", "GESW%"]]
-        
-    # },
-           ]   
+]
