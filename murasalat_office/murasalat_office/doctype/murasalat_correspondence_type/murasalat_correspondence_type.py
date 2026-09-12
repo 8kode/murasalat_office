1815bd1a986b0cac51a38976b5875448c411a566
alqudami <8kode8@gmail.com>
2026-09-12 04:21:42 +0300

    all good before put internal as department

---
+++ murasalat_office/murasalat_office/doctype/murasalat_correspondence_type/murasalat_correspondence_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Murasalat Office and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MurasalatCorrespondenceType(Document):
+	pass
